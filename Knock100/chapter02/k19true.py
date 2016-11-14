4a454b700779b745fe5f4ca430fb6b917c53d714
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-14 19:14:35 +0900

    19を誤って解釈していたので修正（完成）

--- Knock100/chapter02/k19true.py
+++ Knock100/chapter02/k19true.py
@@ -1,9 +1,12 @@
 import re
 from collections import defaultdict
+from operator import itemgetter
 from sys import argv
 
 lines = []
 with open(argv[1], 'r') as f:
     l = [re.split('[ \t]', line)[0] for line in f.readlines()]
     dic = {k: l.count(k) for k in l}
-    print('\n'.join(['{0} {1}'.format(v, k) for k, v in sorted(dic.items(), key=lambda x: x[1], reverse=True)]))
+    print('\n'.join(['   {0} {1}'.format(v, k) for k, v in sorted(dic.items(), key=itemgetter(1, 0), reverse=True)]))
+
+    # uniqコマンドで個数を出すと頭に半角スペース3つはいるので
