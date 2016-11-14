7e34df00143eed7bfbba2418c3c085a0aaccab24
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-15 00:57:40 +0900

    k34エラーがでる

---
+++ Knock100/chapter04/k34.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+import json
+
+with open('out.json') as fi:
+    sentences = json.load(fi)
+    # nouns_sa = [sentence[i - 1]['surface'] + morpheme['surface'] + sentence[i + 1]['surface']
+    #             for sentence in sentences for i, morpheme in zip(range(len(sentence)), sentence)
+    #             if len(morpheme) != 0 and morpheme['pos1'] == '連体化' and morpheme['base'] == 'の']
+    nouns_sa = []
+    j = 0
+    for sentence in sentences:
+        for i, morpheme in zip(range(len(sentence)), sentence):
+            j += 1
+            if len(morpheme) != 0 and morpheme['pos1'] == '連体化' and morpheme['base'] == 'の':
+                nouns_sa.append(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
+            print(j)
+    print(nouns_sa)
+    print(len(nouns_sa))
