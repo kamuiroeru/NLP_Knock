bec11ce331b9a49d6982a4d1c6c6be47c310a45e
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-14 18:58:02 +0900

    19を誤って解釈していたので修正

--- Knock100/chapter02/k19true.py
+++ Knock100/chapter02/k19true.py
@@ -2,20 +2,8 @@ import re
 from collections import defaultdict
 from sys import argv
 
-
 lines = []
 with open(argv[1], 'r') as f:
-    lines = f.readlines()
-
-dic = defaultdict(list)
-for i, token in enumerate([re.split('[ \t]+', line)[0] for line in lines]):
-    dic[token].append(i)
-
-# newList = []
-# for key, value in sorted(dic.items(), key=lambda x: len(x[1]), reverse=True):
-#     for index in value:
-#         newList.append(lines[index])
-newList = [lines[index] for key, value in sorted(dic.items(), key=lambda x: len(x[1]), reverse=True) for index in value]
-
-for l in newList:
-    print(l, end='')
+    l = [re.split('[ \t]', line)[0] for line in f.readlines()]
+    dic = {k: l.count(k) for k in l}
+    print('\n'.join(['{0} {1}'.format(v, k) for k, v in sorted(dic.items(), key=lambda x: x[1], reverse=True)]))
