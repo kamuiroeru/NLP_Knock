eafa23854b9ebe0b8452e68b725a8b3496f0fb4c
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-15 00:22:38 +0900

    k32動詞の原形OK

--- Knock100/chapter04/k32.py
+++ Knock100/chapter04/k32.py
@@ -4,6 +4,6 @@ import json
 with open('out.json') as fi:
     sentences = json.load(fi)
     verbs = [morpheme['surface'] for sentence in sentences for morpheme in sentence
-             if len(morpheme) != 0 and morpheme['pos'] == '動詞']
-    pprint(verbs)
+             if len(morpheme) != 0 and morpheme['pos'] == '動詞' and morpheme['surface'] == morpheme['base']]
+    print(verbs)
     # print(len(verbs))
