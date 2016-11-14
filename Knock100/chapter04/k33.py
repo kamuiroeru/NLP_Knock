871b762e0fb5a245bb8fae256b59d7ed6bce42f4
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-15 00:29:51 +0900

    k33サ変接続の名詞OK  なんかゴミが入ってる(--)

---
+++ Knock100/chapter04/k33.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+import json
+
+with open('out.json') as fi:
+    sentences = json.load(fi)
+    nouns_sa = [morpheme['surface'] for sentence in sentences for morpheme in sentence
+             if len(morpheme) != 0 and morpheme['pos1'] == 'サ変接続']
+    print(nouns_sa)
+    print(len(nouns_sa))
