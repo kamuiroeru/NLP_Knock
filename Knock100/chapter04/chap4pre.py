6b9a8551cb38636d00b142365399a85f1109bab3
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-14 23:25:33 +0900

    30開始 メモリバカ食いしてやばい

--- Knock100/chapter04/chap4pre.py
+++ Knock100/chapter04/chap4pre.py
@@ -1,10 +1,12 @@
 import MeCab
 
-m = MeCab.Tagger('-Ochasen')
+m = MeCab.Tagger('mecabrc')
 
 fi = open('neko.txt')
-fo = open('neko.txt.mecab', 'a')
+fo = open('neko.txt.mecab', 'w')
 
 with fi, fo:
+    s = ''
     for line in fi.readlines():
-        print(m.parse(line.strip()), file=fo)
+        s += m.parse(line.strip())
+    fo.write(s)
--- Knock100/chapter04/mecabPractice.py
+++ Knock100/chapter04/mecabPractice.py
@@ -1,8 +1,13 @@
 import MeCab
 
-m = MeCab.Tagger('-Ochasen')
+# m = MeCab.Tagger('-Ochasen')
+m = MeCab.Tagger('mecabrc')
+# mecabrcの場合の出力フォーマット
+# 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
 
 while(True):
     print('>>>', end='')
     s = m.parse(str(input()))
+    n = m.parseToNode(str(input()))
     print(s)
+    print(n.surface + ','.join(n.feature))
