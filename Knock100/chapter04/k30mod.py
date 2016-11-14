c89ba26fa867f5f3f1ebcd40cab9a9f59e871e64
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-14 23:43:33 +0900

    jsonにしたりして遊ぶ

---
+++ Knock100/chapter04/k30mod.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+import sys
+import re
+
+#     0    1         2          3          4     5     6    7   8   9
+# 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+
+outer_list = []
+inner_list = []
+with open(sys.argv[1]) as fi:
+    for line in fi.readlines():
+        mapping = {}
+        element = re.split('[,\t]', line.strip())
+        if element[0] == 'EOS' or len(element) < 2:
+            inner_list.append({})
+            outer_list.append(inner_list)
+            inner_list = []
+            continue
+        mapping['surface'] = element[0]
+        mapping['base'] = element[7]
+        mapping['pos'] = element[1]
+        mapping['pos1'] = element[2]
+        inner_list.append(mapping)
+pprint(outer_list)
