56bcddde53bc84b79f292b1e1fb1651f0e71c728
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-15 00:16:08 +0900

    k31、動詞のsurfaceをすべて抽出 OK

---
+++ Knock100/chapter04/k31.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+import json
+
+with open('out.json') as fi:
+    sentences = json.load(fi)
+    verbs = [morpheme['surface'] for sentence in sentences for morpheme in sentence
+             if len(morpheme) != 0 and morpheme['pos'] == '動詞']
+    pprint(verbs)
