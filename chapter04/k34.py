aa5621d7cc78ad362fd984a0a9570457af8fc7e5
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-15 20:45:49 +0900

    dict変数名の改善

--- chapter04/k34.py
+++ chapter04/k34.py
@@ -5,19 +5,19 @@ with open('out.json') as fi:
     sentences = json.load(fi)
 
     start = time()
-    nouns_sa = [sentence[i - 1]['surface'] + morpheme['surface'] + sentence[i + 1]['surface']
-                for sentence in sentences for i, morpheme in zip(range(len(sentence)), sentence)
-                if morpheme['pos1'] == '連体化' and morpheme['base'] == 'の']
+    a_and_b = [sentence[i - 1]['surface'] + morpheme['surface'] + sentence[i + 1]['surface']
+               for sentence in sentences for i, morpheme in zip(range(len(sentence)), sentence)
+               if morpheme['pos1'] == '連体化' and morpheme['base'] == 'の']
     t1 = time()
 
-    nouns_sa2 = []
+    a_and_b2 = []
     for sentence in sentences:
         for i, morpheme in zip(range(len(sentence)), sentence):
             if morpheme['pos1'] == '連体化' and morpheme['base'] == 'の':
-                nouns_sa2.append(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
+                a_and_b2.append(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
     t2 = time()
 
-    print(nouns_sa2)
-    print(len(nouns_sa2))
+    print(a_and_b2)
+    print(len(a_and_b2))
     print('{} [sec]'.format(t1 - start))
     print('{} [sec]'.format(t2 - t1))
--- chapter04/k35.py
+++ chapter04/k35.py
@@ -3,6 +3,11 @@ import json
 with open('out.json') as fi:
     sentences = json.load(fi)
     nouns_sa = [morpheme['surface'] for sentence in sentences for morpheme in sentence
-                if morpheme['pos1'] == 'サ変接続']
+                if morpheme['pos'] == '名詞']
+    nouns_sa = []
+    for sentence in sentences:
+
+        for morpheme in sentence:
+
     print(nouns_sa)
     print(len(nouns_sa))
