29b6d92ecba5827dd9d21017d477fa466344d3a4
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-15 09:52:41 +0900

    k34OK 動作確認が取れたのでリスト内包表記に変更

--- Knock100/chapter04/k34.py
+++ Knock100/chapter04/k34.py
@@ -3,17 +3,17 @@ import json
 
 with open('out.json') as fi:
     sentences = json.load(fi)
-    # nouns_sa = [sentence[i - 1]['surface'] + morpheme['surface'] + sentence[i + 1]['surface']
-    #             for sentence in sentences for i, morpheme in zip(range(len(sentence)), sentence)
-    #             if len(morpheme) != 0 and morpheme['pos1'] == '連体化' and morpheme['base'] == 'の']
-    nouns_sa = []
-    j = 0
-    for sentence in sentences:
-        for i, morpheme in zip(range(len(sentence)), sentence):
-            j += 1
-            if len(morpheme) != 0 and morpheme['pos1'] == '連体化' and morpheme['base'] == 'の':
-                nouns_sa.append(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
-                print(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
-                # print(j)
+    nouns_sa = [sentence[i - 1]['surface'] + morpheme['surface'] + sentence[i + 1]['surface']
+                for sentence in sentences for i, morpheme in zip(range(len(sentence)), sentence)
+                if len(morpheme) != 0 and morpheme['pos1'] == '連体化' and morpheme['base'] == 'の']
+    # nouns_sa = []
+    # j = 0
+    # for sentence in sentences:
+    #     for i, morpheme in zip(range(len(sentence)), sentence):
+    #         j += 1
+    #         if len(morpheme) != 0 and morpheme['pos1'] == '連体化' and morpheme['base'] == 'の':
+    #             nouns_sa.append(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
+    #             print(sentence[i - 1]['surface'] + sentence[i]['surface'] + sentence[i + 1]['surface'])
+    #             print(j)
     print(nouns_sa)
     print(len(nouns_sa))
