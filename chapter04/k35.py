ec2381c089dc165d3f23797bfc9fdd4f540bd827
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-16 10:15:58 +0900

    k35 連接名詞のみ抽出に変更 完成

--- chapter04/k35.py
+++ chapter04/k35.py
@@ -2,12 +2,22 @@ import json
 
 with open('out.json') as fi:
     sentences = json.load(fi)
-    nouns_sa = [morpheme['surface'] for sentence in sentences for morpheme in sentence
-                if morpheme['pos'] == '名詞']
-    nouns_sa = []
+    # nouns_sequences = [ ''.join([morpheme['surface'] for morpheme in sentence if morpheme['pos'] == '名詞'])
+    #                    for sentence in sentences]
+    nouns_sequences = []
+    is_continuing = False
     for sentence in sentences:
-
+        nouns_sequence = []
         for morpheme in sentence:
+            if morpheme['pos'] == '名詞':
+                is_continuing = True
+            else:
+                if len(nouns_sequence) >= 2:  # 2単語以上繋がったものだけ取り出す
+                    nouns_sequences.append(''.join(nouns_sequence))
+                nouns_sequence = []
+                is_continuing = False
+            if is_continuing:
+                nouns_sequence.append(morpheme['surface'])
 
-    print(nouns_sa)
-    print(len(nouns_sa))
+    print(nouns_sequences)
+    print(len(nouns_sequences))
