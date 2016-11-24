5c198e55c3eb3f41f4a5aaeda83683d82a642d6e
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-24 23:43:52 +0900

    原形を数えるように改良

--- chapter04/k36.py
+++ chapter04/k36.py
@@ -2,12 +2,13 @@ import json
 from collections import Counter
 
 # from resource import getrusage,RUSAGE_SELF
+from operator import itemgetter
 
 with open('out.json') as fi:
     # print(getrusage(RUSAGE_SELF).ru_maxrss)
     sentence = json.load(fi)
     total = len(sentence)
-    for word, count in Counter([morpheme['base'] for morpheme in sentence]).most_common():
+    for word, count in sorted(Counter([morpheme['base'] for morphbeme in sentence]).most_common(), key=itemgetter(1,0), reverse=True):
         print(word, count/total)
         # print(word)
         # print(getrusage(RUSAGE_SELF).ru_maxrss)
