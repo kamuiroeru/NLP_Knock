5c71115117e7bcf4ea39b7edcce8cf13cb7b30e2
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-24 17:51:57 +0900

    出力を変更

--- chapter04/k33.py
+++ chapter04/k33.py
@@ -1,9 +1,11 @@
 import json
+from pprint import pprint
 
 with open('out.json') as fi:
     sentences = json.load(fi)
-    nouns_sa = [morpheme['surface'] for sentence in sentences for morpheme in sentence
+    nouns_sa = [morpheme for sentence in sentences for morpheme in sentence
                 if morpheme['pos1'] == 'サ変接続']
+    # pprint(nouns_sa)
     for noun_sa in nouns_sa:
-        print(noun_sa)
+        pprint(noun_sa)
     # print(len(nouns_sa))
