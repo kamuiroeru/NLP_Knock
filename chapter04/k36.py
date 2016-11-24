f16bea235d5010b1662ae47d6e4e997f0655dbdf
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-24 19:44:13 +0900

    細かな調整

--- chapter04/k36.py
+++ chapter04/k36.py
@@ -1,11 +1,13 @@
 import json
 from collections import Counter
+
 # from resource import getrusage,RUSAGE_SELF
 
 with open('out.json') as fi:
     # print(getrusage(RUSAGE_SELF).ru_maxrss)
-    # sentence = json.load(fi)
-    for word, count in Counter([morpheme['surface'] for morpheme in json.load(fi)]).most_common():
-        print(word, count)
-    # print(getrusage(RUSAGE_SELF).ru_maxrss)
-
+    sentence = json.load(fi)
+    total = len(sentence)
+    for word, count in Counter([morpheme['base'] for morpheme in sentence]).most_common():
+        print(word, count/total)
+        # print(word)
+        # print(getrusage(RUSAGE_SELF).ru_maxrss)
