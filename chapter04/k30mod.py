2e500cb8a24ad0a9493f2149bd9393f7dc20237f
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-24 18:07:46 +0900

    jsonの出力を変更

--- chapter04/k30mod.py
+++ chapter04/k30mod.py
@@ -1,11 +1,11 @@
 from pprint import pprint
 import sys
 import json
-import k30
+import k30true
 
 #     0    1         2          3          4     5     6    7   8   9
 # 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
-dic = k30.load_mecab(sys.argv[1])
+dic = k30true.load_mecab(sys.argv[1])
 pprint(dic)
 with open(sys.argv[2], 'w') as fo:
     json.dump(dic, fo)
---
+++ chapter04/k30true.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+import sys
+import re
+
+
+#     0    1         2          3          4     5     6    7   8   9
+# 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+
+def load_mecab(strin=''):
+    outer_list = []  # 全文リスト
+    with open(strin) as fi:
+        for line in fi.readlines():
+            mapping = {}
+            element = re.split('[,\t]', line.strip())
+            if element[0] != 'EOS':
+                mapping['surface'] = element[0]  # 表層形
+                mapping['base'] = element[7]  # 原形（基本形）
+                mapping['pos'] = element[1]  # 品詞
+                mapping['pos1'] = element[2]  # 品詞細分類1
+                outer_list.append(mapping)
+    return outer_list
+
+
+if __name__ == '__main__':
+    pprint(load_mecab(sys.argv[1]))
