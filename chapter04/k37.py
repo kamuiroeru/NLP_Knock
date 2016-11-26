1bf7bc00728298076c1d958f0e48fe12c2d2ac81
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-26 14:11:14 +0900

    変数名をよりわかりやすいものに変更

--- chapter04/k37.py
+++ chapter04/k37.py
@@ -7,11 +7,11 @@ import seaborn as sns
 with open('out.json') as fi:
     sentence = json.load(fi)
     total = len(sentence)
-    sorted_list = sorted(Counter([morpheme['base'] for morpheme in sentence]).most_common(), key=itemgetter(1, 0),
+    sorted_list_top10 = sorted(Counter([morpheme['base'] for morpheme in sentence]).most_common(), key=itemgetter(1, 0),
                          reverse=True)[:10]
 
-data = pd.DataFrame({'品詞': [l[0] for l in sorted_list],
-                     '出現頻度': [l[1] / total for l in sorted_list]})
+data = pd.DataFrame({'品詞': [l[0] for l in sorted_list_top10],
+                     '出現頻度': [l[1] / total for l in sorted_list_top10]})
 # sns.plt.ylim([0, 0.1]) #y軸の最大値設定
 sns.set_context("notebook", 1.3)  # 文字の大きさ変更
 sns.set_palette("hot", 10)  # 色変更
