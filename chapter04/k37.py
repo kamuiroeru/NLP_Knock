f8bc56f04cfbb5fc18fa20773a4cdac7608e4d4c
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-26 14:05:16 +0900

    プログラムのいらない部分削除

--- chapter04/k37.py
+++ chapter04/k37.py
@@ -1,28 +1,20 @@
 import json
 from collections import Counter
-
-# from resource import getrusage,RUSAGE_SELF
 from operator import itemgetter
-
 import pandas as pd
 import seaborn as sns
 
 with open('out.json') as fi:
-    # print(getrusage(RUSAGE_SELF).ru_maxrss)
     sentence = json.load(fi)
     total = len(sentence)
     sorted_list = sorted(Counter([morpheme['base'] for morpheme in sentence]).most_common(), key=itemgetter(1, 0),
                          reverse=True)[:10]
-    # for word, count in sorted_list:
-    #     print(word, count / total)
-    # print(word)
-    # print(getrusage(RUSAGE_SELF).ru_maxrss)
 
 data = pd.DataFrame({'品詞': [l[0] for l in sorted_list],
                      '出現頻度': [l[1] / total for l in sorted_list]})
-# sns.plt.ylim([0, 0.1])
-sns.set_context("notebook", 1.3)
-sns.set_palette("hot", 10)
+# sns.plt.ylim([0, 0.1]) #y軸の最大値設定
+sns.set_context("notebook", 1.3)  # 文字の大きさ変更
+sns.set_palette("hot", 10)  # 色変更
 sns.barplot(x='品詞', y='出現頻度', data=data)
 sns.plt.title('頻出Top10')
 sns.plt.show()
