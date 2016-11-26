a98e39bb48c3b02cfa3855c054ac00944f2b2242
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-11-26 14:52:49 +0900

    k39作成

---
+++ chapter04/k39.py
@@ -0,0 +1,23 @@
+import json
+from collections import Counter
+from operator import itemgetter
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+with open('out.json') as fi:
+    sentence = json.load(fi)
+    total = len(sentence)
+    sorted_list = sorted(Counter([morpheme['base'] for morpheme in sentence]).most_common(), key=itemgetter(1, 0),
+                         reverse=True)
+
+data = pd.DataFrame({'単語': [l[0] for l in sorted_list],
+                     '出現頻度': [l[1] / total for l in sorted_list],
+                     '順位': [i for i in range(len(sorted_list))]})
+sns.set_context("notebook", 1.3)  # 文字の大きさ変更
+# sns.set_palette("hot", 10)  # 色変更plt.xscale('log')
+plt.xscale('log')
+plt.yscale('log')
+plt.plot('順位', '出現頻度', data=data)
+plt.title('ヒストグラム')
+plt.show()
