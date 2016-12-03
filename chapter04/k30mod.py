abd2bd7a2eec0be0daf4fc8ff7c1da547936232f
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-03 19:30:33 +0900

    +alpha実験

--- chapter04/k30mod.py
+++ chapter04/k30mod.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 import sys
 import json
+
+import pickle
+
 import k30true
 
 #     0    1         2          3          4     5     6    7   8   9
@@ -9,3 +12,4 @@ dic = k30true.load_mecab(sys.argv[1])
 pprint(dic)
 with open(sys.argv[2], 'w') as fo:
     json.dump(dic, fo)
+    # pickle.dump(dic, open('pic.pickle', 'wb'))
