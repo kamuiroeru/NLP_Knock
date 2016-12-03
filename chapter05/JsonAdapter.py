a04f53e14745b36cb25e111a9db75ac26e533158
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-03 16:01:04 +0900

    命名規則をpip8に

--- chapter05/JsonAdapter.py
+++ chapter05/JsonAdapter.py
@@ -7,39 +7,38 @@ import json
 
 class JsonAdapter(object):
     # jsonからシリアライズ
-    def Serialize(self, argJsonData) :
+    def serialize(self, arg_json_data):
 
-        jsonData = argJsonData
-        if isinstance(argJsonData, basestring ) :
-            jsonData = json.loads(argJsonData)
+        json_data = arg_json_data
+        if isinstance(arg_json_data, str):
+            json_data = json.loads(arg_json_data)
 
         for key in self.__dict__.keys():
-            if isinstance(getattr(self,key), JsonAdapter ) or getattr(self,key) == None :
-                valclass = getattr(self,key)
-                if valclass == None :
-                    valclass = getattr(sys.modules["Classes.DataPDO"],key)()
+            if isinstance(getattr(self, key), JsonAdapter) or not getattr(self, key):
+                val_class = getattr(self, key)
+                if not val_class:
+                    val_class = getattr(sys.modules["Classes.DataPDO"], key)()
 
-                valclass.Serialize(jsonData[key])
-                setattr(self,key, valclass)
+                val_class.serialize(json_data[key])
+                setattr(self, key, val_class)
 
-            elif isinstance(getattr(self,key), int ) :
-                setattr(self,key, int(jsonData[key]))
-            else :
-                setattr(self,key, jsonData[key])
+            elif isinstance(getattr(self, key), int):
+                setattr(self, key, int(json_data[key]))
+            else:
+                setattr(self, key, json_data[key])
 
     # Jsonを出力
-    def ToJson(self):
-        jsonDict = self.__ToDictionary()
-        jsonstring = json.dumps(jsonDict, ensure_ascii=False)
-        return jsonstring
+    def to_json(self):
+        json_dict = self.to_dictionary()
+        json_string = json.dumps(json_dict, ensure_ascii=False)
+        return json_string
 
-    #Mapを作成
-    def __ToDictionary(self):
-        jsonDict = {}
+    # Mapを作成
+    def to_dictionary(self):
+        json_dict = {}
         for key in self.__dict__.keys():
-            if isinstance(getattr(self,key), JsonAdapter ) :
-                jsonDict.update({key : getattr(self,key).__ToDictionary()})
-            else :
-                jsonDict.update({key : getattr(self,key)})
-        return jsonDict
-
+            if isinstance(getattr(self, key), JsonAdapter):
+                json_dict.update({key: getattr(self, key).to_dictionary()})
+            else:
+                json_dict.update({key: getattr(self, key)})
+        return json_dict
