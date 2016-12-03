5a2912247daf3b26b7de8bcdc3d3755970839f39
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-03 20:42:44 +0900

    ただの整形

--- chapter05/MyClass.py
+++ chapter05/MyClass.py
@@ -4,3 +4,10 @@ class Morph:
         self.base = ''
         self.pos = ''
         self.pos1 = ''
+
+
+class Chunk:
+    def __init__(self):
+        self.morphs = Morph()
+        self.dst = 0
+        self.srcs = 0
