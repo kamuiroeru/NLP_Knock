a9f034167612120f9430e46e063c7b4430ba5591
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-03 23:23:00 +0900

    完成（main関数はpickleファイル読み込む仕様に変更）

--- chapter05/k41.py
+++ chapter05/k41.py
@@ -1,26 +1,54 @@
 import re
+from collections import defaultdict
+from makePickle import pickleLoad
+
+import pandas as pd
 
 from MyClass import Morph
+from MyClass import Chunk
 
 
-def create_morph(cabocha_text_lattice='neko.txt.cabocha'):
+def create_chunk(cabocha_text_lattice='neko.txt.cabocha'):
     outer_list = []
     inner_list = []
+    temp_dic = defaultdict(list)
+    chunk = Chunk()
+    index = -1
     with open(cabocha_text_lattice) as f:
-        for line in filter(lambda x: x[0] is not '*', f):
-            elements = re.split('[,\t]', line.rstrip())
-            if len(elements) == 1:
-                outer_list.append(inner_list)
-                inner_list = []
+        for line in f:
+            status = line.split(' ')
+            if len(status) == 5:  # latticeの行なら
+                chunk.srcs = temp_dic[str(index)]
+                inner_list.append(chunk)
+                index += 1
+                chunk = Chunk()
+                chunk.dst = int(status[2][:-1])  # D抜いてintにキャスト
+                temp_dic[str(chunk.dst)].append(index)  # 係り元を集計
             else:
-                morph = Morph()
-                morph.surface = elements[0]
-                morph.base = elements[7]
-                morph.pos = elements[1]
-                morph.pos1 = elements[2]
-                inner_list.append(morph)
+                if 'EOS' in line:  # EOSの時
+                    chunk.srcs = temp_dic[str(index)]
+                    inner_list.append(chunk)
+                    outer_list.append(inner_list[1:])
+                    chunk = Chunk()
+                    inner_list = []
+                    temp_dic = defaultdict(list)
+                    index = -1
+                else:
+                    elements = re.split('[,\t]', line.rstrip())
+                    morph = Morph()
+                    morph.surface = elements[0]
+                    morph.base = elements[7]
+                    morph.pos = elements[1]
+                    morph.pos1 = elements[2]
+                    chunk.morphs.append(morph)
     return outer_list
 
 
 if __name__ == '__main__':
-    print(list(map(lambda x: x.surface, create_morph()[3])))
+    chunk8 = pickleLoad('outchunk.pickle')[7]
+    df = pd.DataFrame(
+        [[''.join(list(map(lambda x: x.surface, chunk.morphs))),
+          chunk.dst,
+          chunk.srcs] for chunk in chunk8],
+        columns=['文節', '係り先', '係り元リスト'])
+    print(df)
