aa3b3f34b6be8cc03370dd4f18367100fb7f83d0
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-03 20:09:28 +0900

    pickle使うことにした（ちょっとしたライブラリ書いた）

---
+++ chapter05/makePickle.py
@@ -0,0 +1,26 @@
+import gzip
+import pickle
+
+
+def pickleDump(obj=[], filename='', comp=False, level=1):
+    pickle_str = pickle.dumps(obj)
+
+    if comp:  # 圧縮指定された時
+        gzip.open(filename + '.gz', 'wb').write(gzip.compress(pickle_str, compresslevel=level))
+        return
+
+    with open(filename + '.pickle', 'wb') as f:
+        f.write(pickle_str)
+
+
+def pickleLoad(filename=''):
+    if '.gz' not in filename or '.pickle' not in filename:
+        print('対応していないファイルです。')
+
+    if '.gz' in filename:  # 圧縮されてた時
+        with gzip.open(filename) as f:
+            return pickle.loads(f.read())
+
+    else:
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
