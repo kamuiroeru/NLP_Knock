f21c92f8b408160b0e1f57f22e97e1ca6277a392
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-03 20:37:01 +0900

    表示もするようにした

--- chapter05/k40makepic.py
+++ chapter05/k40makepic.py
@@ -1,4 +1,6 @@
-from makePickle import pickleDump
+import makePickle as mp
 from k40 import create_morph
 
-pickleDump(create_morph(), 'out.pickle')
+mp.pickleDump(create_morph(), 'out')
+dic = mp.pickleLoad('out.pickle')
+print(list(map(lambda x: x.surface, dic[3])))
