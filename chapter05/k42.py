ee8a40cd43aba433fd33b6f9bb2e3594eae86c62
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-08 23:32:42 +0900

    記号を除去した

--- chapter05/k42.py
+++ chapter05/k42.py
@@ -4,14 +4,24 @@ from makePickle import pickleLoad
 
 input_sentence = pickleLoad('outchunk.pickle')
 
+# pprint(input_sentence)
+
 output_sentence = []
 for bun in input_sentence:
+    if not bun:
+        continue
     string = ''
+    strings_list = []
     for chunks in bun:
-        if chunks.dst != -1:
-            origin = ''.join([morph.surface for morph in chunks.morphs])
-            origin += ''.join([morph.surface for morph in bun[chunks.dst].morphs])
-            string += origin + '\t'
+        if chunks.dst == -1:
+            continue
+        origin = ''.join([morph.surface for morph in chunks.morphs if not morph.pos == '記号'])
+        end = ''.join([morph.surface for morph in bun[chunks.dst].morphs if not morph.pos == '記号'])
+        strings_list.append(origin + end)
+    string = '\t'.join(strings_list)
     output_sentence.append(string)
 
-pprint(output_sentence)
+# pprint(output_sentence)
+
+for sentence in output_sentence:
+    print(sentence)
