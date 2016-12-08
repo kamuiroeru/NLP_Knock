f1e8245de989489dccc4647c2a5b839f8af82468
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-08 23:50:25 +0900

    コメント追加

--- chapter05/k43.py
+++ chapter05/k43.py
@@ -16,6 +16,7 @@ for bun in input_sentence:
         if chunks.dst == -1 \
                 or '名詞' not in [morph.pos for morph in chunks.morphs] \
                 or '動詞' not in [morph.pos for morph in bun[chunks.dst].morphs]:
+            # 係り先が無い時 or 係り元に「名詞」を含まない時 or 係り先に「動詞」を含まない時
             continue
         origin = ''.join([morph.surface for morph in chunks.morphs if not morph.pos == '記号'])
         end = ''.join([morph.surface for morph in bun[chunks.dst].morphs if not morph.pos == '記号'])
