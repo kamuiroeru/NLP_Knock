4ad4f2f89d200891833aa370485e21e593426875
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-08 20:38:05 +0900

    とりあえず実装 記号を除去はまだ

--- chapter05/k42.py
+++ chapter05/k42.py
@@ -1,3 +1,17 @@
+from pprint import pprint
+
 from makePickle import pickleLoad
 
-chunk = pickleLoad('outchunk.pickle')
+input_sentence = pickleLoad('outchunk.pickle')
+
+output_sentence = []
+for bun in input_sentence:
+    string = ''
+    for chunks in bun:
+        if chunks.dst != -1:
+            origin = ''.join([morph.surface for morph in chunks.morphs])
+            origin += ''.join([morph.surface for morph in bun[chunks.dst].morphs])
+            string += origin + '\t'
+    output_sentence.append(string)
+
+pprint(output_sentence)
