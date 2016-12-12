cafd39aa95fe020a30d8ef1993078a6289ba9b14
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-12 22:53:07 +0900

    コメント

--- chapter05/k48.py
+++ chapter05/k48.py
@@ -1,13 +1,13 @@
 from makePickle import pickleLoad
 
 for bun in pickleLoad('outchunk.pickle'):
-    if len(bun) <= 1:  # bunが空（[]）の時
-        continue
+    if len(bun) <= 1:  # bun の中の文節が1節以内のとき
+        continue  # スキップ
     for chunks in bun:
-        if '名詞' in [morph.pos for morph in chunks.morphs]:
+        if '名詞' in [morph.pos for morph in chunks.morphs]:  # 文節に名詞が入っている時
             tree_path = [''.join([morph.surface for morph in chunks.morphs if not morph.pos == '記号'])]
             index = chunks.dst
-            while index != -1:
+            while index != -1:  # 係り先がなくなるまで
                 tree_path.append(''.join([morph.surface for morph in bun[index].morphs if not morph.pos == '記号']))
                 index = bun[index].dst
             print(' -> '.join(tree_path))
