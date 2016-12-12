4d45df2d8e57b60ad3ff72a9c816b042f09ca04f
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-12 20:29:13 +0900

    コメント

--- chapter05/k45.py
+++ chapter05/k45.py
@@ -8,9 +8,11 @@ for bun in pickleLoad('outchunk.pickle'):
             continue
         elif '動詞' in [morph.pos for morph in chunks.morphs]:
             predicate = [morph.base for morph in chunks.morphs if morph.pos == '動詞'][0]
-            cases = []
-            for chunks2 in [bun[src] for src in chunks.srcs]:
+            cases = []  # 係り元の助詞が入るリスト
+            for chunks2 in [bun[src] for src in chunks.srcs]:  # 係り元全部に対して文節ごとに
+
+                # 記号を取り除いて（表層形, 品詞）のタプル in リストを作る
                 case = [(morph.surface, morph.pos) for morph in chunks2.morphs if not morph.pos == '記号']
-                if case and case[-1][1] == '助詞':  #
-                    cases.append(case[-1][0])
+                if case and case[-1][1] == '助詞':  # 末尾が助詞だった場合、
+                    cases.append(case[-1][0])  # その助詞の表層形を取り出してリストに
             print(predicate + '\t' + ' '.join(sorted(cases)))
