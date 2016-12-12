afad2a3d1c919ef192eafe0a7db0605cb49ef176
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-12 16:45:19 +0900

    完成

--- chapter05/k42.py
+++ chapter05/k42.py
@@ -1,20 +1,12 @@
 from makePickle import pickleLoad
 
-input_sentence = pickleLoad('outchunk.pickle')
-
-output_sentence = []
-for bun in input_sentence:
+for bun in pickleLoad('outchunk.pickle'):
     if not bun:  # bunが空（[]）の時
         continue
     strings_list = []
     for chunks in bun:
         if chunks.dst == -1:  # 係り先が無い時
             continue
-        origin = ''.join([morph.surface for morph in chunks.morphs if not morph.pos == '記号'])  # 記号を除去
-        end = ''.join([morph.surface for morph in bun[chunks.dst].morphs if not morph.pos == '記号'])  # 上に同じ
-        strings_list.append(origin + end)
-    string = '\t'.join(strings_list)  # タブ区切りで文字列にする
-    output_sentence.append(string)
-
-for sentence in output_sentence:
-    print(sentence)
+        print(''.join([morph.surface for morph in chunks.morphs if not morph.pos == '記号'])
+              + '\t'
+              + ''.join([morph.surface for morph in bun[chunks.dst].morphs if not morph.pos == '記号']))
