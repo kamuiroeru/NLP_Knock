c470bf3758e62bfb272cef1c1c2c53057a8a578e
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-13 21:16:12 +0900

    add

---
+++ chapter05/k49second.py
@@ -0,0 +1,13 @@
+from makePickle import pickleLoad
+
+for bun in pickleLoad('outchunk.pickle'):
+    if len(bun) <= 1:  # bun の中の文節が1節以内のとき
+        continue  # スキップ
+    for chunk in bun:
+        if '名詞' in [morph.pos for morph in chunk.morphs]:  # 文節に名詞が入っている時
+            tree_path = [''.join([morph.surface for morph in chunk.morphs if not morph.pos == '記号'])]
+            index = chunk.dst
+            while index != -1:  # 係り先がなくなるまで
+                tree_path.append(''.join([morph.surface for morph in bun[index].morphs if not morph.pos == '記号']))
+                index = bun[index].dst
+            print(' -> '.join(tree_path))
