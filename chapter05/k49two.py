243092cfc4391785d1403149b903c45b8fabb062
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-14 22:48:56 +0900

    変数名変更

--- chapter05/k49two.py
+++ chapter05/k49two.py
@@ -13,10 +13,10 @@ for bun in pickleLoad('outchunk.pickle'):
                     else:
                         noun_from += morph.surface
             until_path = []  # 途中の経路を溜めておく
-            index = chunk.dst
-            while index != -1:  # 係り先がなくなるまで
-                check_chunk = [(morph.surface, morph.pos) for morph in bun[index].morphs]
-                if '名詞' in [morph.pos for morph in bun[index].morphs]:
+            dst_index = chunk.dst
+            while dst_index != -1:  # 係り先がなくなるまで
+                check_chunk = [(morph.surface, morph.pos) for morph in bun[dst_index].morphs]
+                if '名詞' in [morph.pos for morph in bun[dst_index].morphs]:
                     print(' -> '.join([noun_from]+until_path+['Y']))
-                until_path.append(''.join([morph.surface for morph in bun[index].morphs]))
-                index = bun[index].dst
+                until_path.append(''.join([morph.surface for morph in bun[dst_index].morphs]))
+                dst_index = bun[dst_index].dst
