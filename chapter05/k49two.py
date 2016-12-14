bd86b52aada3a0cc75631c84339ddcdc99ce50cd
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-14 22:59:04 +0900

    連続したXや「〜X」とかを「X」に統合

--- chapter05/k49two.py
+++ chapter05/k49two.py
@@ -1,3 +1,5 @@
+import re
+
 from makePickle import pickleLoad
 
 for bun in pickleLoad('outchunk.pickle'):
@@ -12,11 +14,12 @@ for bun in pickleLoad('outchunk.pickle'):
                         noun_from += 'X'
                     else:
                         noun_from += morph.surface
+            noun_from = re.sub('.+X', 'X', noun_from)  # Xの重なりとかをまとめる
             until_path = []  # 途中の経路を溜めておく
             dst_index = chunk.dst
             while dst_index != -1:  # 係り先がなくなるまで
                 check_chunk = [(morph.surface, morph.pos) for morph in bun[dst_index].morphs]
                 if '名詞' in [morph.pos for morph in bun[dst_index].morphs]:
-                    print(' -> '.join([noun_from]+until_path+['Y']))
-                until_path.append(''.join([morph.surface for morph in bun[dst_index].morphs]))
+                    print(' -> '.join([noun_from] + until_path + ['Y']))
+                until_path.append(''.join([morph.surface for morph in bun[dst_index].morphs if not morph.pos == '記号']))
                 dst_index = bun[dst_index].dst
