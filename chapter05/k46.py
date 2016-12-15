87cbfe77538ece625402bc16c22611d34b96a710
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-15 12:11:46 +0900

    動詞にかかる助詞がない場合を除去

--- chapter05/k46.py
+++ chapter05/k46.py
@@ -16,6 +16,7 @@ for bun in pickleLoad('outchunk.pickle'):
                 if case and case[-1][1] == '助詞':  # caseが[]の場合を弾くために case and かます
                     cases.append([case[-1][0], ''.join([c[0] for c in case])])  # [助詞の表層形, 助詞を含む文節]
             cases.sort(key=itemgetter(0, 1))  # 助詞の表層形でソート
-            print(predicate + '\t'
-                  + ' '.join([case[0] for case in cases]) + '\t'  # 助詞
-                  + ' '.join([case[1] for case in cases]))  # 助詞を含む文節
+            if cases:
+                print(predicate + '\t'
+                      + ' '.join([case[0] for case in cases]) + '\t'  # 助詞
+                      + ' '.join([case[1] for case in cases]))  # 助詞を含む文節
