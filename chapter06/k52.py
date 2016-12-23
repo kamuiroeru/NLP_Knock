821176d87d6506af4c7c69ba299da6fe9fc23d81
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-23 14:08:49 +0900

    完成

--- chapter06/k52.py
+++ chapter06/k52.py
@@ -1,1 +1,9 @@
+from nltk import stem, re
+
 with open('nlp51.out') as f:
+    st = stem.PorterStemmer()
+    for word in f:
+        word = word.rstrip()
+        if word:
+            word_for_stem = re.sub('[^a-z]', '', word.lower())
+            print(word + '\t' + st.stem(word_for_stem))
