36fc1b06ea464523faa08887f80c8c5307c85338
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-31 18:37:49 +0900

    外部xmlを読み込む方式に変更 外部モジュールparseを使用

--- chapter06/k53.py
+++ chapter06/k53.py
@@ -1,12 +1,14 @@
-import json
-from pprint import pprint
+import re
+from sys import argv
+from parse import parse
 
-import corenlp
+try:
+    raw_text_directory = argv[1]
+except IndexError:
+    raw_text_directory = 'nlp.txt.xml'
 
-corenlp_dir = '/usr/local/lib/stanford-corenlp-full-2016-10-31/'
-parser = corenlp.StanfordCoreNLP(corenlp_path=corenlp_dir, properties='hoge.properities')
-
-obj = parser.parse('Natural language processing (NLP) is a field of computer science, artificial intelligence, and linguistics concerned with the interactions between computers and human (natural) languages. As such, NLP is related to the area of humani-computer interaction. Many challenges in NLP involve natural language understanding, that is, enabling computers to derive meaning from human or natural language input, and others involve natural language generation.')
-print(obj)
-# result_json = json.loads(parser.parse('Natural language processing (NLP) is a field of computer science, artificial intelligence, and linguistics concerned with the interactions between computers and human (natural) languages. As such, NLP is related to the area of humani-computer interaction. Many challenges in NLP involve natural language understanding, that is, enabling computers to derive meaning from human or natural language input, and others involve natural language generation.'))
-# pprint(result_json)
+for line in open(raw_text_directory):
+    line = line.strip()
+    word = parse('<word>{}</word>', line)
+    if word:
+        print(word[0])
