b78ed9e4e91473f9fd0fba8717e2be2c80b76e1f
KamuiRoeru <e1371@s.akashi.ac.jp>
2016-12-31 20:32:25 +0900

    完成

--- chapter06/k55.py
+++ chapter06/k55.py
@@ -1,19 +1,17 @@
 from sys import argv
-from parse import parse
+import xml.etree.ElementTree as ET
 
 try:
     raw_text_directory = argv[1]
 except IndexError:
     raw_text_directory = 'nlp.txt.xml'
 
-for line in open(raw_text_directory):
-    line = line.strip()
-    word = parse('<word>{}</word>', line)
-    lemma = parse('<lemma>{}</lemma>', line)
-    pos = parse('<POS>{}</POS>', line)
-    if word:
-        print(word[0] + '\t', end='')
-    elif lemma:
-        print(lemma[0] + '\t', end='')
-    elif pos:
-        print(pos[0])
+tree = ET.parse(raw_text_directory)
+words = tree.iter('word')
+poss = tree.iter('POS')
+ners = tree.iter('NER')
+
+for obj in zip(words, poss, ners):
+    obj = [ob.text for ob in obj]
+    if obj[1] == 'NNP' and obj[2] == 'PERSON':
+        print(obj[0])
