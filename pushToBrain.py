a4e57a7a432dd0f9cd2c432b9e499745a0d6d0db
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-12 17:39:43 +0900

    ディレクトリ変更に伴う変更

--- pushToBrain.py
+++ pushToBrain.py
@@ -4,5 +4,5 @@ from subprocess import run
 
 os.chdir('/Users/rokumura/Documents/programming/Knock100/toBrain')
 run(['git', 'add', '.'])
-run(['git', 'commit', '-m', ' "PushToBrain' + datetime.now().strftime("%Y_%m_%d %H-%M-%S") + '"'])
+run(['git', 'commit', '-m', 'PushToBrain' + datetime.now().strftime("%Y_%m_%d %H-%M-%S")])
 run(['git', 'push', 'origin', 'master'])
