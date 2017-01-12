18d4a176e84c77eeb1e921f1f9ce556f1b9563db
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-12 17:25:32 +0900

    ディレクトリ変更に伴う変更

--- pushToBrain.py
+++ pushToBrain.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-os.chdir('/Documents/programming/Knock100/toBrain')
+os.chdir('~/Documents/programming/Knock100/toBrain')
 os.system('git add .')
 os.system('git commit -m "PushToBrain' + datetime.now().strftime("%Y_%m_%d %H-%M-%S") + '"')
 os.system('git push origin master')
