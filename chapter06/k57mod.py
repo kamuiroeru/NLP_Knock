6bde7a4a8073d2bee18e4059ef7a7b3f33010362
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-15 02:28:46 +0900

    係り受けの形式を表示するようにした 完成、コメント追加、整形 xmltodict使う方式 tokenが1つしか無い（13文目）に対応

--- chapter06/k57mod.py
+++ chapter06/k57mod.py
@@ -13,10 +13,19 @@ dependencies = [sentences['dependencies'][1] for sentences in load_xml()['root']
 G = Digraph(format='png', )
 G.attr('node', shape='circle')
 
+brk = False
 for dep in dependencies[n]['dep']:
+
+    if isinstance(dep, str):  # 13文目用の処理
+        dep = dependencies[n]['dep']
+        brk = True
+
     dd, dg = dep['dependent'], dep['governor']  # 文字数削減
     G.node(dd['@idx'], dd['#text'])  # 同じ英単語でも違う場所にあったりするので
     G.node(dg['@idx'], dg['#text'])  # ちゃんとノードを作る
     G.edge(dd['@idx'], dg['@idx'], label=dep['@type'])  # 矢印作る
+
+    if brk:  # 13文目用の処理
+        break
 
 G.render('node/' + str(n + 1), view=True, cleanup=True)
