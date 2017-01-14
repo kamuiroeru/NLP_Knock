3bc010e95c7cf3ed166769820967907f898d61d9
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-14 23:44:51 +0900

    完成 xmltodict使う方式

--- chapter06/k55mod.py
+++ chapter06/k55mod.py
@@ -1,17 +1,17 @@
-from sys import argv
-import xml.etree.ElementTree as ET  # 内部モジュールのxml読み込み機能使用
+from k54 import load_xml
 
-try:
-    raw_text_directory = argv[1]
-except IndexError:
-    raw_text_directory = 'nlp.txt.xml'
 
-tree = ET.parse(raw_text_directory)  # xmlファイルを読み込んでElementTreeオブジェクトに
-words = tree.iter('word')  # wordの要素だけ取ってきてリストに
-poss = tree.iter('POS')  # POSの（ｒｙ
-ners = tree.iter('NER')  # NERの（ｒｙ
+def print_person_name(word='', pos='', ner=''):
+    if pos == 'NNP' and ner == 'PERSON':
+        print(word)
 
-for obj in zip(words, poss, ners):
-    texts = [ob.text for ob in obj]  # テキストだけほしいので抽出
-    if texts[1] == 'NNP' and texts[2] == 'PERSON':
-        print(texts[0])
+for sentence in load_xml()['root']['document']['sentences']['sentence']:
+    tokens = sentence['tokens']['token']
+
+    # tokenが1つしか無い時の処理
+    if isinstance(tokens, dict):
+        print_person_name(tokens['word'], tokens['POS'], tokens['NER'])
+        continue
+
+    for t in tokens:
+        print_person_name(t['word'], t['POS'], t['NER'])
