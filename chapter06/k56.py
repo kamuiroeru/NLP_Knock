2e8c042d75d11ee1388af48e564e4081bdca5201
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-14 23:34:34 +0900

    完成コメント追加

--- chapter06/k56.py
+++ chapter06/k56.py
@@ -1,38 +1,33 @@
-import pickle
-
-dic = pickle.load(open('xml.pickle', 'rb'))
-
-sentences = dic['root']['document']['sentences']
+from k54 import load_xml
+import re
 
 # sentence_list[sentenceIndex][tokenIndex]で任意の単語が取得できるようなリストを作成
-sentence_list = [[]]  # インデックス調整の為
-for sentence in sentences['sentence']:
-    # print(sentence['@id'])
-    token_list = ['']  # インデックス調整の為
+sentence_list = [[]]  # インデックス調整の為予め1つ要素を入れておく
+
+for sentence in load_xml()['root']['document']['sentences']['sentence']:
+    token_list = ['']  # インデックス調整の為（ｒｙ
     if isinstance(sentence['tokens']['token'], dict):
-        # print(sentence['tokens']['token']['word'])
         token_list.append(sentence['tokens']['token']['word'])
     else:
         for token in sentence['tokens']['token']:
-            # print(token['word'])
             token_list.append(token['word'])
     sentence_list.append(token_list)
 
-for core in dic['root']['document']['coreference']['coreference']:
+for core in load_xml()['root']['document']['coreference']['coreference']:
     rep = ''
     for mention in core['mention']:
-        if '@representative' in mention:
-            # print(mention['@representative'])
+        if '@representative' in mention:  # 代表参照表現なら文字列を取得
             rep = (mention['text'])
         else:
             sent = int(mention['sentence'])
             start = int(mention['start'])
             end = int(mention['end'])
+
+            # 参照表現の先頭に '「 [代表表現] ( ' を追加
             sentence_list[sent][start] = '「 ' + rep + ' ( ' + sentence_list[sent][start]
+
+            # 参照表現の 1つ後ろ の先頭に ')」' を追加
             sentence_list[sent][end] = ')」' + sentence_list[sent][end]
 
 for token_l in sentence_list[1:]:
-    print(' '.join(token_l[1:]))
-    # for token in token_l:
-    #     print(token[1]+token[0], end='')
-    # print()
+    print(re.sub(r' ([,.;:?!])', r'\1', ' '.join(token_l[1:])))  # カンマやピリオドの前の空白を削除
