c3fcfce92c63767d7ff177f59044822d7d236ac0
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-15 00:45:56 +0900

    完成、コメント追加、整形 xmltodict使う方式

--- chapter06/k57mod.py
+++ chapter06/k57mod.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-from sys import argv
-import xml.etree.ElementTree as ET  # 内部モジュールのxml読み込み機能使用
 from graphviz import Digraph
 from k54 import load_xml
 from sys import argv
@@ -10,41 +7,16 @@ try:
 except IndexError:
     n = 0
 
+# collapsed-dependenciesは2番目
 dependencies = [sentences['dependencies'][1] for sentences in load_xml()['root']['document']['sentences']['sentence']]
 
 G = Digraph(format='png', )
 G.attr('node', shape='circle')
-pprint(dependencies[1]['dep'])
+
 for dep in dependencies[n]['dep']:
-    dd, dg = dep['dependent'], dep['governor']
-    G.node(dd['@idx'], dd['#text'])
-    G.node(dg['@idx'], dg['#text'])
-    G.edge(dd['@idx'], dg['@idx'])
+    dd, dg = dep['dependent'], dep['governor']  # 文字数削減
+    G.node(dd['@idx'], dd['#text'])  # 同じ英単語でも違う場所にあったりするので
+    G.node(dg['@idx'], dg['#text'])  # ちゃんとノードを作る
+    G.edge(dd['@idx'], dg['@idx'])  # 矢印作る
 
 G.render('node/' + str(0), view=True, cleanup=True)
-#     # dep = dependency['dep']
-#     print(dependency)
-#     # G.node(dep)
-# try:
-#     raw_text_directory = argv[1]
-# except IndexError:
-#     raw_text_directory = 'nlp.txt.xml'
-#
-# tree = ET.parse(raw_text_directory)  # xmlファイルを読み込んでElementTreeオブジェクトに
-#
-# sentences = []
-# for dependencie in tree.iter('dependencies'):
-#     if dependencie.attrib['type'] == 'collapsed-dependencies':
-#         sentence = []
-#         for dep in dependencie:
-#             sentence.append([(d.text, d.get('idx')) for d in dep])
-#         sentences.append(sentence)
-# pprint(sentences)
-#
-# G = Digraph(format='png', )
-# G.attr('node', shape='circle')
-# for dep in sentences[0]:
-#     h = [d[0] for d in dep][::-1]
-#     G.edge(*h)
-#
-# G.render('node/' + str(0), view=True, cleanup=True)
