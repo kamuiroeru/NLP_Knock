46ceeb466cca744e8726b2181863da14f964573e
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-14 20:32:22 +0900

    add

---
+++ chapter06/k56.py
@@ -0,0 +1,42 @@
+import pickle
+
+dic = pickle.load(open('xml.pickle', 'rb'))
+
+sentences = dic['root']['document']['sentences']
+
+# sentence_list[sentenceIndex][tokenIndex]で任意の単語が取得できるようなリストを作成
+sentence_list = [[]]  # インデックス調整の為
+for sentence in sentences['sentence']:
+    # print(sentence['@id'])
+    token_list = ['']  # インデックス調整の為
+    if isinstance(sentence['tokens']['token'], dict):
+        # print(sentence['tokens']['token']['word'])
+        token_list.append(sentence['tokens']['token']['word'])
+    else:
+        for token in sentence['tokens']['token']:
+            # print(token['word'])
+            token_list.append(token['word'])
+    sentence_list.append(token_list)
+
+for core in dic['root']['document']['coreference']['coreference']:
+    rep = ''
+    for mention in core['mention']:
+        if '@representative' in mention:
+            # print(mention['@representative'])
+            rep = (mention['text'])
+        else:
+            sent = int(mention['sentence'])
+            start = int(mention['start'])
+            end = int(mention['end'])
+            # print(sent, start)
+            sentence_list[sent][start] = '「 ' + rep + ' ( ' + sentence_list[sent][start]
+            sentence_list[sent][end] = ')」' + sentence_list[sent][end]
+    #     print(mention['text'])
+    # print()
+
+for lc, token_l in enumerate(sentence_list):
+    if lc:
+        print(' '.join(token_l))
+    # for token in token_l:
+    #     print(token[1]+token[0], end='')
+    # print()
