a21d240545f5addbbf3936d9cbf6ce08e48b6652
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-14 23:04:44 +0900

    完成とコメント追加

--- chapter06/k54.py
+++ chapter06/k54.py
@@ -6,8 +6,11 @@ sentences = dic['root']['document']['sentences']['sentence']
 
 for sentence in sentences:
     tokens = sentence['tokens']['token']
+
+    # tokenが1つしか無い時の処理
     if isinstance(tokens, dict):
         print(tokens['word'], tokens['lemma'], tokens['POS'])
         continue
+
     for t in tokens:
         print(t['word'], t['lemma'], t['POS'])
