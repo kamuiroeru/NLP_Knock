12c984c8224b37c7357bad485b3a84ec75668a5e
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-19 20:33:21 +0900

    完成

--- chapter07/k60.py
+++ chapter07/k60.py
@@ -1,11 +1,11 @@
 import leveldb
 import json
 
-db = leveldb.LevelDB()
+db = leveldb.LevelDB('./lvdb')
 
 artistDic = {}
 for line in open('artist.json'):
     if '"area"' in line.strip():
         inDic = json.loads(line)
-        db.Put(inDic['name'], inDic['area'])
-        artistDic[inDic['name']] = inDic['area']
+        db.Put(inDic['name'].encode(), inDic['area'].encode())
+        # artistDic[inDic['name']] = inDic['area']
