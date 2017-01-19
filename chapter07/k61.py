f4e4ef3dc4b13c28ed2bd8d64524bd94ea603eaa
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-19 20:33:56 +0900

    add

---
+++ chapter07/k61.py
@@ -0,0 +1,16 @@
+import leveldb
+
+db = leveldb.LevelDB('./lvdb')
+
+while True:
+    try:
+        key = input('>>>')
+        if '--all' in key:
+            for k, v in db.RangeIter():
+                print(k.decode() + ' : ' + v.decode())
+        else:
+            print(db.Get(key.encode()).decode())
+    except KeyError:
+        print('Error:そのKeyは存在しません')
+    except KeyboardInterrupt or EOFError:
+        exit()
