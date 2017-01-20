6de1e498fb14af90eb38890ef17c6c44c57cce46
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-20 12:10:18 +0900

    完成 コメント追加

--- chapter07/k66.py
+++ chapter07/k66.py
@@ -1,9 +1,9 @@
 # MongoDBのインタラクティブシェルでは
-# find({"area":"Japan"}).count()
+# find({"area":"Japan"}).count() または count({"area":"Japan"})
 # を実行する
 
 import k65
 
 if __name__ == '__main__':
     query = {"area": "Japan"}
-    print(k65.col.count(query))
+    print(k65.col.count(query))  # count関数を使う
