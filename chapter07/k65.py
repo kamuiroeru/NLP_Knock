47b35dbf458e4ede4bd59618183b101a2872a976
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-20 12:07:05 +0900

    完成 コメント追加 他のスクリプトでcolを呼び出すため、 main関数に主要処理を移行

--- chapter07/k65.py
+++ chapter07/k65.py
@@ -4,6 +4,7 @@ client = pymongo.MongoClient()
 db = client['my']  # データベース名 補完が効くのでこちらの書き方がおすすめ
 col = db['nlp100']  # コレクション名
 
-query = {'name': 'Queen'}
-for data in col.find(query):
-    print(data)
+if __name__ == '__main__':
+    query = {'name': 'Queen'}
+    for data in col.find(query):
+        print(data)
