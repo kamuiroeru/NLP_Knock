09ad037152331b7511ce8d5efbb29db3a135d894
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-20 11:58:14 +0900

    完成 コメント追加

--- chapter07/k65.py
+++ chapter07/k65.py
@@ -1,8 +1,8 @@
 import pymongo
 
 client = pymongo.MongoClient()
-db = client['my']
-col = db['nlp100']
+db = client['my']  # データベース名 補完が効くのでこちらの書き方がおすすめ
+col = db['nlp100']  # コレクション名
 
 query = {'name': 'Queen'}
 for data in col.find(query):
