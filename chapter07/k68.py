6543e89880a8aceed8708b62ec75d1386b2d2103
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-21 20:44:43 +0900

    完成 コメント追加

--- chapter07/k68.py
+++ chapter07/k68.py
@@ -3,9 +3,11 @@ import k65
 if __name__ == '__main__':
     col = k65.col
     query = {'tags.value': 'dance', 'rating': {'$exists': True}}
-    top10 = {}
+    top = {}
     for data in col.find(query):
-        top10[data['name']] = data['rating']['count']
-    top = sorted(top10.items(), key=lambda x: (-x[1], x[0]))[0:10]
-    for lc, name in enumerate(top, start=1):
+        top[data['name']] = data['rating']['count']
+
+    # レーティング数で降順ソート & nameを辞書昇順ソート
+    top10 = sorted(top.items(), key=lambda x: (-x[1], x[0]))[0:10]
+    for lc, name in enumerate(top10, start=1):
         print(lc, name[0])
