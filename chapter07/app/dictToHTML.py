d676af2b5950eabba2584c02c2808d9a150ca3fa
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-22 16:16:23 +0900

    整理

--- chapter07/app/dictToHTML.py
+++ chapter07/app/dictToHTML.py
@@ -1,5 +1,28 @@
+import requests, json, threading
+from bs4 import BeautifulSoup
+from time import sleep
+
+url = ''
+
+
+def getpic(name: str) -> None:
+    global url
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+    payload = {'q': name, 'tbm': 'isch', 'tbs': 'itp:face'}
+    r = requests.get('https://www.google.co.jp/search?', headers=headers, params=payload)
+    while r.status_code != 200:
+        sleep(1)
+        r = requests.get('https://www.google.co.jp/search?', headers=headers, params=payload)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    data = soup.select('div .rg_meta')[0]
+    url = json.loads(str(data.contents[0]))['ou']
+
+
 def convert(d: dict) -> str:
-    # global ret_str
+    name = d['name']
+    thread1 = threading.Thread(target=getpic, args=(name,))
+    thread1.start()
     ret_str = '''<!DOCTYPE html>
                 <html lang="ja">
                 <head>
@@ -9,6 +32,7 @@ def convert(d: dict) -> str:
                 <body>\n'''.format(d['name'])
 
     ret_str += '<h1>{}</h1>\n'.format(d['name'])
+    ret_str += '<table border="0"><tr><td>'
 
     def hoge(lis: list) -> None:
         nonlocal ret_str
@@ -45,6 +69,17 @@ def convert(d: dict) -> str:
 
     d = [(k, v) for k, v in d.items()]
     hoge(d)
+
+    thread1.join()
+    global url
+    ret_str += '''</td>
+        <td valign="top">
+            <a href="{}">
+                <img src="{}" alt="{}" title="{}" height="150">
+            </a>
+        </td>
+    </tr>
+    </table>'''.format(url, url, name, name)
     return ret_str
 
 
--- chapter07/app/main_app.py
+++ chapter07/app/main_app.py
@@ -4,6 +4,7 @@ from bottle import route, run, template, request, static_file, url, get, post, r
 import pymongo, re
 from bson.objectid import ObjectId
 
+# グローバル変数
 sorted_rank = []
 total_count = 0
 col = pymongo.MongoClient()['my']['nlp100']
@@ -14,51 +15,66 @@ def html_index():
     return template('index')
 
 
+@route('/', method='POST')
+def search():
+    artist_name = request.forms.artist_name
+    genre = request.forms.genre
+    area = request.forms.area
+    if artist_name or genre or area:
+        return template(engine(artist_name, genre, area))
+    else:
+        return template('アーティスト名かジャンルを入力してください。<br><a href="/">戻る</a>')
+
+
 @route('/page/<num>')
 def show_other_page(num: str):
     global sorted_rank
     return template(show_part(int(num), sorted_rank))
 
 
-def show_part(start: int, ranking: list) -> str:
+def show_part(page: int, ranking: list) -> str:
     global total_count, col
-    start *= 10
+    start = page * 10
+    part_of_ranking = ranking[start:start + 10]  # rankingの一部
     html_string = str(total_count) + '件ヒット<br><a href="/">トップへ戻る</a>\n'
-    html_string += '<br>{}~{}件を表示<br><ul>'.format(start, start + 10)
-    col = pymongo.MongoClient()['my']['nlp100']
-    for id, count in ranking[start:start + 10]:
-        in_dic = col.find_one({'_id': id})
+    html_string += '<br>{}~{}件を表示<br><ul>'.format(start, start + len(part_of_ranking))
+
+    # リストでstart ~ start+10 個の名前とエリアを表示し、それぞれ詳細ページにリンクする。
+    for idobj, count in part_of_ranking:
+        in_dic = col.find_one({'_id': idobj})
         html_string += '<li><a href="{}" target="_blank">{}</a></li>' \
             .format('/details/' + str(in_dic['_id']), in_dic['name'] + ' : ' + str(in_dic.get('area') or ''))
     html_string += '</ul><br>'
-    start //= 10
-    if not start == 0:
-        html_string += '<a href="{}">前の10件</a>'.format('/page/' + str(start - 1))
-    if len(ranking[start:start + 11]) == 11:
-        html_string += '   <a href="{}">次の10件</a>'.format('/page/' + str(start + 1))
+    if not page == 0:  # 前ページにさらに10件以上あるとき
+        html_string += '<a href="{}">前の10件</a>'.format('/page/' + str(page - 1))
+    if len(ranking[start:start + 11]) == 11:  # 後ページに1件以上あるとき、
+        html_string += '   <a href="{}">次の10件</a>'.format('/page/' + str(page + 1))
     return html_string
 
 
 def engine(name: str, genre: str, area: str) -> str:
-    # var_name = lambda val: [k for k, v in globals().items() if id(v) == id(val)]  # 変数名取得する関数
-    forklist = [['name', name], ['genre', genre], ['area', area]]
-    forklist = [e for e in forklist if e[1]]
-    if len(forklist) > 1:
-        query = {'%or': []}
-        for s in forklist:
+    forklist = [['name', name], ['tags.value', genre], ['area', area]]
+    query = {}
+    for s in forklist:
+        if s[1]:
             if s[0] == 'name':
+                query['$or'] = []
+                # query['name'] = re.compile(name.rstrip(), re.IGNORECASE)
+                # query['aliases.name'] = re.compile(name.rstrip(), re.IGNORECASE)
                 query['$or'].append({'name': re.compile(name.rstrip(), re.IGNORECASE)})
                 query['$or'].append({'aliases.name': re.compile(name.rstrip(), re.IGNORECASE)})
             elif s[0] == 'genre':
-                query['$or'].append({'tags.value': re.compile(genre.rstrip(), re.IGNORECASE)})
+                query['tags.value'] = re.compile(genre.rstrip(), re.IGNORECASE)
+                # query['$or'].append({'tags.value': re.compile(genre.rstrip(), re.IGNORECASE)})
             else:
-                query['$or'].append({'area': re.compile(area.rstrip(), re.IGNORECASE)})
-    else:
-        query = {forklist[0][0]: re.compile(forklist[0][1].rstrip(), re.IGNORECASE)}
+                query['area'] = re.compile(area.rstrip(), re.IGNORECASE)
+                # query['$or'].append({'area': re.compile(area.rstrip(), re.IGNORECASE)})
+    # else:
+    #     query = {forklist[0][0]: re.compile(forklist[0][1].rstrip(), re.IGNORECASE)}
     global total_count, sorted_rank, col
     total_count = col.count(query)
     print(total_count)
-    if total_count > 50:
+    if total_count > 100:
         query['rating.count'] = {'$exists': True}
         ranking = {data['_id']: data['rating']['count'] for data in col.find(query)}
         sorted_rank = sorted(ranking.items(), key=lambda x: x[1], reverse=True)
@@ -71,7 +87,6 @@ def engine(name: str, genre: str, area: str) -> str:
 
 from dictToHTML import convert
 
-
 @route('/details/<idstr>')
 def details(idstr: str):
     global col
@@ -80,17 +95,6 @@ def details(idstr: str):
     html_str = convert(detail_dic)
     html_str += '\n<br><hr><a href="#" onclick="window.close()">[閉じる]</a>\n</body>\n</html>'
     return template(html_str)
-
-
-@route('/', method='POST')
-def search():
-    artist_name = request.forms.artist_name
-    genre = request.forms.genre
-    area = request.forms.area
-    if artist_name or genre or area:
-        return template(engine(artist_name, genre, area))
-    else:
-        return template('アーティスト名かジャンルを入力してください。<br><a href="/">戻る</a>')
 
 
 # @error(404)
