15c8c45298e11243cfd3cb248fdeb63afbdf4ae5
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-01-22 23:15:10 +0900

    完成 コメント追加

--- chapter07/app/main_app.py
+++ chapter07/app/main_app.py
@@ -3,6 +3,8 @@
 from bottle import route, run, template, request, static_file, url, get, post, response, error
 import pymongo, re
 from bson.objectid import ObjectId
+from dictToHTML import convert
+
 
 # グローバル変数
 sorted_rank = []
@@ -10,7 +12,7 @@ total_count = 0
 col = pymongo.MongoClient()['my']['nlp100']
 
 
-@route('/')
+@route('/')  # トップページ
 def html_index():
     return template('index')
 
@@ -26,12 +28,37 @@ def search():
         return template('アーティスト名かジャンルを入力してください。<br><a href="/">戻る</a>')
 
 
-@route('/page/<num>')
-def show_other_page(num: str):
-    global sorted_rank
-    return template(show_part(int(num), sorted_rank))
+# データベースを叩く
+def engine(name: str, genre: str, area: str) -> str:
+    forklist = [['name', name], ['genre', genre], ['area', area]]
+    query = {}
+    for s in forklist:  # 検索語句に応じて適切なクエリを作る
+        if s[1]:
+            if s[0] == 'name':
+                query['$or'] = []
+                query['$or'].append({'name': re.compile(name.rstrip(), re.IGNORECASE)})
+                query['$or'].append({'aliases.name': re.compile(name.rstrip(), re.IGNORECASE)})
+            elif s[0] == 'genre':
+                query['tags.value'] = re.compile(genre.rstrip(), re.IGNORECASE)
+            else:
+                query['area'] = re.compile(area.rstrip(), re.IGNORECASE)
+
+    global total_count, sorted_rank, col
+    hit_data = col.find(query)
+    total_count = hit_data.count()  # 検索個数をカウントして、
+    print(total_count)
+    if total_count > 100:  # 100件を超えてたら
+
+        # レーティングされている項目だけで順位付けする。
+        ranking = {data['_id']: data['rating']['count'] for data in hit_data if data.get('rating')}
+        sorted_rank = sorted(ranking.items(), key=lambda x: x[1], reverse=True)  # レーティング数が多いもの順
+        total_count = len(sorted_rank)  # 検索個数を修正
+    else:
+        sorted_rank = [(data['_id'], data['name']) for data in hit_data]  # そのまま吐き出す
+    return show_part(0, sorted_rank)
 
 
+# 10件ごとに情報を表示
 def show_part(page: int, ranking: list) -> str:
     global total_count, col
     start = page * 10
@@ -42,59 +69,36 @@ def show_part(page: int, ranking: list) -> str:
     # リストでstart ~ start+10 個の名前とエリアを表示し、それぞれ詳細ページにリンクする。
     for idobj, count in part_of_ranking:
         in_dic = col.find_one({'_id': idobj})
-        html_string += '<li><a href="{}" target="_blank">{}</a></li>' \
+        html_string += '<li><a href="{}" target="_blank">{}</a></li>\n' \
             .format('/details/' + str(in_dic['_id']), in_dic['name'] + ' : ' + str(in_dic.get('area') or ''))
     html_string += '</ul><br>'
     if not page == 0:  # 前ページにさらに10件以上あるとき
-        html_string += '<a href="{}">前の10件</a>'.format('/page/' + str(page - 1))
+        html_string += '<a href="{}">前の10件</a>'.format('/page/' + str(page - 1))  # 前の10件ボタン追加
     if len(ranking[start:start + 11]) == 11:  # 後ページに1件以上あるとき、
-        html_string += '   <a href="{}">次の10件</a>'.format('/page/' + str(page + 1))
+        html_string += '   <a href="{}">次の10件</a>'.format('/page/' + str(page + 1))  # 次の10件ボタン追加
     return html_string
 
 
-def engine(name: str, genre: str, area: str) -> str:
-    forklist = [['name', name], ['tags.value', genre], ['area', area]]
-    query = {}
-    for s in forklist:
-        if s[1]:
-            if s[0] == 'name':
-                query['$or'] = []
-                # query['name'] = re.compile(name.rstrip(), re.IGNORECASE)
-                # query['aliases.name'] = re.compile(name.rstrip(), re.IGNORECASE)
-                query['$or'].append({'name': re.compile(name.rstrip(), re.IGNORECASE)})
-                query['$or'].append({'aliases.name': re.compile(name.rstrip(), re.IGNORECASE)})
-            elif s[0] == 'genre':
-                query['tags.value'] = re.compile(genre.rstrip(), re.IGNORECASE)
-                # query['$or'].append({'tags.value': re.compile(genre.rstrip(), re.IGNORECASE)})
-            else:
-                query['area'] = re.compile(area.rstrip(), re.IGNORECASE)
-                # query['$or'].append({'area': re.compile(area.rstrip(), re.IGNORECASE)})
-    # else:
-    #     query = {forklist[0][0]: re.compile(forklist[0][1].rstrip(), re.IGNORECASE)}
-    global total_count, sorted_rank, col
-    total_count = col.count(query)
-    print(total_count)
-    if total_count > 100:
-        query['rating.count'] = {'$exists': True}
-        ranking = {data['_id']: data['rating']['count'] for data in col.find(query)}
-        sorted_rank = sorted(ranking.items(), key=lambda x: x[1], reverse=True)
-        total_count = len(sorted_rank)
-        return show_part(0, sorted_rank)
-    else:
-        sorted_rank = [(data['_id'], data['name']) for data in col.find(query)]
-        return show_part(0, sorted_rank)
+# 前・次の10件が押された時の処理
+@route('/page/<num>')
+def show_other_page(num: str):
+    global sorted_rank
+    return template(show_part(int(num), sorted_rank))
 
 
-from dictToHTML import convert
-
+# 詳細ページを表示
 @route('/details/<idstr>')
 def details(idstr: str):
     global col
+
+    # MongoDBの固有idであるidstrを使って目的の情報をGet
     detail_dic = col.find_one({'_id': ObjectId(idstr)})
-    detail_dic.pop('_id')
-    html_str = convert(detail_dic)
+    detail_dic.pop('_id')  # _idは要らないので除去
+    html_str = convert(detail_dic)  # htmlを作る（dictToHTML.convertに投げる）
     html_str += '\n<br><hr><a href="#" onclick="window.close()">[閉じる]</a>\n</body>\n</html>'
     return template(html_str)
+
+
 
 
 # @error(404)
