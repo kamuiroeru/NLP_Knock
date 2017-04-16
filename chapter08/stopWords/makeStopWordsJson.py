517e20f2004c30b45fb70582b301c38895f4917a
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-04-16 11:46:06 +0900

    [fix] ストップワードリストを作成するscriptを作った
    コメントや細かい修正

--- chapter08/stopWords/makeStopWordsJson.py
+++ chapter08/stopWords/makeStopWordsJson.py
@@ -12,7 +12,7 @@ def ret_extention(fname: str) -> str:
 if __name__ == '__main__':
     outDic = {}
     for fileName in glob('*'):
-        if ret_extention(fileName) != 'py':
+        if ret_extention(fileName) != 'py':  # scriptは除外
 
             if ret_extention(fileName) == 'txt':
                 inList = [line.rstrip() for line in open(fileName)]
@@ -20,9 +20,11 @@ if __name__ == '__main__':
 
             elif ret_extention(fileName) == 'csv':
                 inList = [line.rstrip().split(',') for line in open(fileName)]
-                map(lambda s: s.rstrip(), inList)
+                map(lambda s: s.rstrip(), inList)  # リストのすべての要素に対してrstrip
 
             else:
-                print(fileName + 'のファイル形式は対応していません')
+                print('.' + ret_extention(fileName) + 'のファイル形式は対応していません')
+
+    # jsonをgzipで圧縮して出力
     with gzip.open('JsonStopWordlist.json.gz', 'wb') as f:
         f.write(bytes(json.dumps(outDic), encoding='utf-8'))
