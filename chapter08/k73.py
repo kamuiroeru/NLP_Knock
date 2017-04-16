1a39f545abda07ad841d4270f9d09daed0813340
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-04-16 20:24:31 +0900

    [fix] k73完成
    10行強で書けるロジスティック回帰モデル学習
    を参考に実装
    
    k74へ実装を移植

--- chapter08/k73.py
+++ chapter08/k73.py
@@ -1,9 +1,12 @@
 import collections
 import math
-from k72 import remove_waste
 
 N = 10662  # Change this to present the number of training instances.
 eta0 = 0.1  # Initial learning rate; change this if desired.
+
+
+def sigmoid(x: float) -> float:
+    return 1. / (1. + math.exp(-x))
 
 
 def update(W, X, l, eta):
@@ -11,7 +14,7 @@ def update(W, X, l, eta):
     a = sum([W[x] for x in X])
 
     # Compute the gradient of the error function (avoiding +Inf overflow).
-    g = ((1. / (1. + math.exp(-a))) - l) if -100. < a else (0. - l)
+    g = (sigmoid(a) - l) if -100. < a else (0. - l)
 
     # Update the feature weights by Stochastic Gradient Descent.
     for x in X:
@@ -27,30 +30,3 @@ def train(fi):
         update(W, fields[1:], float(fields[0]), eta0 / (1 + t / float(N)))
         t += 1
     return W
-
-
-if __name__ == '__main__':
-    inputList = []
-    for line in open('sentiment.txt'):
-        lineSplit = line.rstrip().split(' ')
-        inputList.append(' '.join([lineSplit[0]] + remove_waste(lineSplit[1:])))
-    dic = train(inputList)
-    # print(dic)
-
-    border = 0.5
-    correct = 0
-
-    for sentence in inputList:
-        sentList = sentence.split(' ')
-        likelihood = 0.0
-        for word in sentList[1:]:
-            likelihood += dic[word]
-        if likelihood > border:
-            result = '+1'
-        else:
-            result = '-1'
-
-        if sentList[0] == result:
-            correct += 1
-
-    print('正解率：{}'.format(correct / len(inputList)))
