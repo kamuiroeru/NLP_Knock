a5572642b7de64a35ac8e68e0807b0f08c8d7f8a
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-04-23 17:57:06 +0900

    [fix] TP FP TN FN の分岐を修正

--- chapter08/k77.py
+++ chapter08/k77.py
@@ -9,21 +9,21 @@ def classify(instr: str) -> str:
     """
 
     ans, prediction, odds = instr.split('\t')
-    if ans == '+1':
-        return 'TP' if prediction == '+1' else 'FP'
+    if prediction == '+1':
+        return 'TP' if ans == '+1' else 'FP'
     else:
-        return 'TN' if prediction == '-1' else 'FN'
+        return 'TN' if ans == '-1' else 'FN'
 
 
 def ret_score(TP, FP, TN, FN) -> tuple:
     """TP, FP, TN, FNを渡すと、正解率, 適合率, 再現率, F1スコアをタプルで返す関数"""
 
-    rate_of_correct_ans = (TP + TN) / (TP + FP + TN + FN)
+    accuracy = (TP + TN) / (TP + FP + TN + FN)
     precision = TP / (TP + FP)
     recall = TP / (TP + FN)
     F_measure = 2 * precision * recall / (precision + recall)
 
-    return rate_of_correct_ans, precision, recall, F_measure
+    return accuracy, precision, recall, F_measure
 
 
 if __name__ == '__main__':
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     classified = Counter([classify(line.rstrip()) for line in open('k76output.txt')])
     TP, FP, TN, FN = map(lambda x: classified[x], ['TP', 'FP', 'TN', 'FN'])
 
-    rate_of_correct_ans, precision, recall, F_measure = ret_score(TP, FP, TN, FN)
+    accuracy, precision, recall, F_measure = ret_score(TP, FP, TN, FN)
 
-    print('正解率:\t{}'.format(rate_of_correct_ans))
+    print('正解率:\t{}'.format(accuracy))
     print('適合率:\t{}'.format(precision))
     print('再現率:\t{}'.format(recall))
     print('F1スコア:\t{}'.format(F_measure))
