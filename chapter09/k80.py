73748323d31f4f76793aaeef0582002cfacf0a2e
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-04-24 17:53:50 +0900

    [update] コメントや説明を追加。
    f.close()忘れてたので追加。

--- chapter09/k80.py
+++ chapter09/k80.py
@@ -2,6 +2,13 @@
 
 
 def rem_waste(inlis: list) -> iter:
+    """
+    コーパスのリストを読んで記号を除去したイテレータを返す関数
+    
+    stripでコーパスの両端の記号を取り除く処理をリスト内全てのコーパスに適用(map)
+    filterで空文字列を除去
+    """
+
     return filter(lambda s: s != '', map(lambda t: t.strip('.,!?;:()[]"').strip("'"), inlis))
 
 
@@ -11,3 +18,5 @@ with open('out80.txt', 'w') as fo:
     for line in f:
         line = line.rstrip().split()
         fo.write(' '.join(rem_waste(line)) + '\n')
+
+f.close()
