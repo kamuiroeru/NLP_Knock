34f1e4b8ddf2c40bcf23463d719c81a155aaab08
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-04-25 17:03:24 +0900

    [fix] stripの指定についてバグがあったので、修正
    完成、コメント等追加

--- chapter09/k80.py
+++ chapter09/k80.py
@@ -9,14 +9,19 @@ def rem_waste(inlis: list) -> iter:
     filterで空文字列を除去
     """
 
-    return filter(lambda s: s != '', map(lambda t: t.strip('.,!?;:()[]"').strip("'"), inlis))
+    return filter(lambda s: s != '', map(lambda t: t.strip('`.,!?;:()[]\'"“”‘’：；‘,'), inlis))
 
 
 f = open('./enwiki-20150112-400-r10-105752.txt')
 
 with open('out80.txt', 'w') as fo:
-    for line in f:
+    for lc, line in enumerate(f):
+        if lc % 1000 == 0:
+            print('lc: ' + str(lc))
         line = line.rstrip().split()
         fo.write(' '.join(rem_waste(line)) + '\n')
 
+        # デバッグ用
+        # if lc > 200:
+        #     break
 f.close()
