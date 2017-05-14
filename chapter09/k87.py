dcf4c795c03c7d263f844ed7cba9026a13dc3b3f
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-05-14 19:29:03 +0900

    [update] 87アップデート
    コマンドライン引数で任意の文字列を評価できるようにした

--- chapter09/k87.py
+++ chapter09/k87.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pickle
-
+from sys import argv
 
 def cosine(x, y):
     """2つのnumpy配列からCosine類似度を返す"""
@@ -10,9 +10,17 @@ def cosine(x, y):
     return x.dot(y) / normX / normY
 
 
+str1 = 'United_States'
+str2 = 'U.S'
+
+if len(argv) == 3:
+    str1 = argv[1]
+    str2 = argv[2]
+
 result = np.load('result.npz')
 label = pickle.load(open('label.pkl', 'rb'))
 
-x = result[label['United_States']]
-y = result[label['U.S']]
-print(cosine(x, y))
+x = result[label[str1]]
+y = result[label[str2]]
+print('「{}」と「{}」のcosine類似度は{:0.2f}'
+      .format(str1, str2, cosine(x, y)))
