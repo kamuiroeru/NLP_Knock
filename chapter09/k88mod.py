89c0d2fd28e288a0fb9c7f26664b010b5920b43f
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-05-14 20:53:01 +0900

    [add] 特異値分解用88

---
+++ chapter09/k88mod.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pickle
+from k87 import cosine
+
+decompMat = np.load('decompMat.npy')
+label = pickle.load(open('label.pkl', 'rb'))
+
+array = decompMat[label['England']]
+
+def cul(k, v):
+    return k, cosine(array, decompMat[v])
+
+def procedure():
+    from multiprocessing import Pool
+    with Pool() as p:
+        ret = p.starmap(cul, label.items())
+    return ret
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        array = decompMat[label[argv[1]]]
+
+    checkDic = procedure()
+
+    best10 = sorted(checkDic, key=lambda x: x[1], reverse=True)[1:11]
+
+    for word, cos in best10:
+        print('{}\t\t{:0.2f}'.format(word, cos))
