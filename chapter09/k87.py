f65b7a0d9160af95ffb2165b07edfcbc11268d2f
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-05-14 20:52:24 +0900

    [fix] cosine関数のゼロ割を回避
    特異値分解に変更

--- chapter09/k87.py
+++ chapter09/k87.py
@@ -4,10 +4,12 @@ from sys import argv
 
 def cosine(x, y):
     """2つのnumpy配列からCosine類似度を返す"""
-
-    normX = np.linalg.norm(x)
-    normY = np.linalg.norm(y)
-    return x.dot(y) / normX / normY
+    if x.any() and y.any():
+        normX = np.linalg.norm(x)
+        normY = np.linalg.norm(y)
+        return x.dot(y) / normX / normY
+    else:
+        return 0
 
 
 if __name__ == '__main__':
@@ -18,10 +20,10 @@ if __name__ == '__main__':
         str1 = argv[1]
         str2 = argv[2]
 
-    result = np.load('result.npy')
+    decompMat = np.load('decompMat.npy')
     label = pickle.load(open('label.pkl', 'rb'))
 
-    x = result[label[str1]]
-    y = result[label[str2]]
+    x = decompMat[label[str1]]
+    y = decompMat[label[str2]]
     print('「{}」と「{}」のcosine類似度は{:0.2f}'
           .format(str1, str2, cosine(x, y)))
