80a9596d590633419358904707e6d309b2c77b0f
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-05-28 20:17:00 +0900

    [update] コメント追加（完成）

--- chapter10/k91.py
+++ chapter10/k91.py
@@ -2,7 +2,7 @@
 
 # import json
 instr = open('./questions-words.txt').read()
-instr
+# instr
 sections = [section.split('\n') for section in instr.split(':')]
 sectionsDic = {l[0].strip(): l[1:] for l in sections}
 
