b5cf551d346e5624a9a622cc0c5264c8b6dfb2da
KamuiRoeru <e1371@s.akashi.ac.jp>
2017-06-10 17:33:26 +0900

    [fix] 国名リストの作成方法の見直し
    国名リストでvectorに含まれない語句を除去するのを忘れてたのを修正

--- chapter10/k96.py
+++ chapter10/k96.py
@@ -3,12 +3,14 @@ from k90gensim import model
 keys = [line.rstrip().replace(' ', '_') for line in open('../chapter09/useList.txt')]
 
 vectors = []
+newkeys = []
 for key in keys:
     try:
         vectors.append(model[key])
+        newkeys.append(key)
     except KeyError:
         pass
 
 import pickle
-pickle.dump(keys, open('countryNames.pkl', 'wb'))
+pickle.dump(newkeys, open('countryNames.pkl', 'wb'))
 pickle.dump(vectors, open('countryVectors.pkl', 'wb'))
